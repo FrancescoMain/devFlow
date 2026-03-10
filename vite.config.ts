4bb7a0165094661cc6b50ffe4ca5ca99fd14ae05
francesco.cesarano <cesaranofrancescomain@gmail.com>
2026-03-10 17:22:28 +0100

    tailwind setup + path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 3003 },
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: { port: 3000 },
 });
